2a3a2a86b4df2945aeefce5ae9b90418a1da51ef
Ivo Petkov <48838737+ip681@users.noreply.github.com>
2023-05-26 00:09:22 +0300

    Update 03. Wolf in Sheep's clothing.py

--- 01. Basic Syntax, Conditional Statements and Loops/01. Basic Syntax, Conditional Statements and Loops - More Exercise/03. Wolf in Sheep's clothing.py
+++ 01. Basic Syntax, Conditional Statements and Loops/01. Basic Syntax, Conditional Statements and Loops - More Exercise/03. Wolf in Sheep's clothing.py
@@ -10,3 +10,6 @@ else:
                 print(f"Oi! Sheep number {index}! You are about to be eaten by a wolf!")
     else:
         pass
+
+# sheeps = list(reversed(input().split(', ')))
+# print('Please go away and stop eating my sheep' if sheeps[0] == 'wolf' else f'Oi! Sheep number {sheeps.index("wolf")}! You are about to be eaten by a wolf!')
