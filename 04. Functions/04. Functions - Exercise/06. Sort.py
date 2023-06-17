08cec467bbc771b30da7a5684165972ec2271594
Ivo Petkov <48838737+ip681@users.noreply.github.com>
2023-06-17 11:24:28 +0300

    func

--- 04. Functions/04. Functions - Exercise/06. Sort.py
+++ 04. Functions/04. Functions - Exercise/06. Sort.py
@@ -2,11 +2,11 @@ main_list = [int(n) for n in input().split()]
 sort_list = list()
 
 
-def sorted(numbers):
+def sorted_l(numbers):
     numbers.sort()
     for n in numbers:
         sort_list.append(n)
     return sort_list
 
 
-print(sorted(main_list))
+print(sorted_l(main_list))
