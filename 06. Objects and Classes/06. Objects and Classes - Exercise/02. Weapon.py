e5217f387a6dcd1dba414dd48e8af1f92a5d1c9a
Ivo Petkov <48838737+ip681@users.noreply.github.com>
2023-07-30 00:40:41 +0300

    Create 02. Weapon.py

---
+++ 06. Objects and Classes/06. Objects and Classes - Exercise/02. Weapon.py
@@ -0,0 +1,15 @@
+class Weapon:
+
+    def __init__(self, bullets):
+        self.bullets = int(bullets)
+
+    def shoot(self):
+        if self.bullets - 1 >= 0:
+            self.bullets -= 1
+            return "shooting..."
+        else:
+            return "no bullets left"
+
+    def __repr__(self):
+        return f"Remaining bullets: {self.bullets}"
+
