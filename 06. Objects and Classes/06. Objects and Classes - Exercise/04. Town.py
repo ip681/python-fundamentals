23ee129d2609a6aed5a4e5c627a5630d96cc1c70
Ivo Petkov <48838737+ip681@users.noreply.github.com>
2023-08-11 00:41:00 +0300

    Create 04. Town.py

---
+++ 06. Objects and Classes/06. Objects and Classes - Exercise/04. Town.py
@@ -0,0 +1,16 @@
+class Town:
+
+    def __init__(self, name):
+        self.name = name
+        self.latitude = "0°N"
+        self.longitude = "0°E"
+
+    def set_latitude(self, latitude):
+        self.latitude = latitude
+
+    def set_longitude(self, longitude):
+        self.longitude = longitude
+
+    def __repr__(self):
+        return f"Town: {self.name} | Latitude: {self.latitude} | Longitude: {self.longitude}"
+
