77c1ef23c97e0f0ea431043d582e740ec18a118e
Ivo Petkov <48838737+ip681@users.noreply.github.com>
2023-08-20 11:10:26 +0300

    Create 07. SoftUni Parking.py

---
+++ 07. Dictionaries/07. Dictionaries - Exercise/07. SoftUni Parking.py
@@ -0,0 +1,34 @@
+number_cars = int(input())
+parking_info = {}
+
+
+def register_car(name, number):
+    if name in parking_info:
+        print(f"ERROR: already registered with plate number {parking_info[name]}")
+        return
+    parking_info[name] = number
+    print(f"{name} registered {number} successfully")
+
+
+def unregister_car(name):
+    if name not in parking_info:
+        print(f"ERROR: user {name} not found")
+        return
+    del parking_info[name]
+    print(f"{name} unregistered successfully")
+
+
+def all_cars_in_praking():
+    for key, value in parking_info.items():
+        print(f"{key} => {value}")
+
+
+for _ in range(number_cars):
+    command, name, *number = input().split()
+    if command == "register":
+        number = number[-1]
+        register_car(name, number)
+    else:
+        unregister_car(name)
+
+all_cars_in_praking()
