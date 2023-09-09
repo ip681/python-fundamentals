86988923e31a0e9e32e27d97901d9e8abb013b75
Ivo Petkov <48838737+ip681@users.noreply.github.com>
2023-09-09 10:23:43 +0300

    Create 04. Caesar Cipher.py

---
+++ 08. Text Processing/08. Text Processing - Exercise/04. Caesar Cipher.py
@@ -0,0 +1,10 @@
+print("".join([chr(ord(letter) + 3) for letter in input()]))
+
+
+# main_text = input()
+# result = ""
+# for letter in main_text:
+#     cripted_letter = ord(letter)
+#     result += chr(cripted_letter + 3)
+#
+# print(result)
